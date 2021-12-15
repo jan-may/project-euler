ad20143f3d62735a49c78c89040ab65baab02b92
Jan May <jan_may@gmx.net>
2021-12-15 15:06:44 +0100

    #10

---
+++ e010.py
@@ -0,0 +1,14 @@
+#https://projecteuler.net/problem=10
+
+'''
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+'''
+
+import eulerlib
+
+def sum_of_primes(n):
+    return sum(eulerlib.primes(n))
+
+if __name__ == '__main__':   
+    print(sum_of_primes(2000000))
